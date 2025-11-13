261433f2227d1fa545798bcfcce2a81046c79f84
Mauricio123-bravo <hectormauriciobravo@gmail.com>
2025-11-13 18:20:19 -0500

    first commit

--- populate.js
+++ populate.js
@@ -7,7 +7,7 @@ import Job from "./models/JobModel.js";
 import User from "./models/UserModel.js";
 try {
   await mongoose.connect(process.env.MONGO_URL);
-  const user = await User.findOne({ email: "mao@gmail.com" });
+  const user = await User.findOne({ email: "test@test.com" });
 
   const jsonJobs = JSON.parse(
     await readFile(new URL("./utils/mockData.json", import.meta.url))
